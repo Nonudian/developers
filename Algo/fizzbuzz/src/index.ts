be2172ea6d3a6b1a2b99b7d3c605c4dedbe3d82f
William <william.chazot@gmail.com>
2021-11-16 19:26:08 +0100

    add fizzbuzz algorithm

--- Algo/fizzbuzz/src/index.ts
+++ Algo/fizzbuzz/src/index.ts
@@ -1,1 +1,32 @@
-console.log('Hello');
+
+
+/** Fizzbuzz rules */
+const rules = [
+    { divisor: 3, display: 'Fizz' },
+    { divisor: 5, display: 'Buzz' }
+];
+
+
+/** Main function. We assume that number param is positive */
+function fizzbuzz(n: number): Array<string | number> {
+    return recursiveFizzbuzz(n).reverse();
+}
+
+/** Check whether a number is divisible by a given divisor */
+function isDivisibleBy(number: number, divisor: number) {
+    return number % divisor === 0;
+}
+
+/** Recursive function of fizzbuzz algorithm, that stores values before displaying */
+function recursiveFizzbuzz(n: number): Array<string | number> {
+    const string = rules
+        .filter(({ divisor }) => isDivisibleBy(n, divisor))
+        .map(({ display }) => display)
+        .join('');
+
+    return [string || n, ...(n > 1 ? recursiveFizzbuzz(n - 1) : [])];
+}
+
+
+/** Just only one test is necessary, I mean... */
+console.log(fizzbuzz(42));
